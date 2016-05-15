97db4b5b53da471a9915672a1b950ffa1b187e20
444832384@qq.com <444832384@qq.com>
2016-05-15 23:58:04 +0900

    Html5mobiledevelop

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * [test js run demo]
+ * @param  {[type]} i [description]
+ * @return {[type]}   [description]
+ */
+var  select = $('.sw');
+$('.sw')(0).addClass(select);
+for (var i = 0; i < select.length; i++) {
+
+}
